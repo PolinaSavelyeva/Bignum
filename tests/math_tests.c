591cd7737c353a8d95fed475ab985ffad54c07b8
Polina Savelyeva <polina.a.savelyeva@gmail.com>
2024-02-28 01:39:16 +0300

    Add defs to math tests

--- tests/math_tests.c
+++ tests/math_tests.c
@@ -5,71 +5,36 @@
 #include <stdlib.h>
 #include "string_op.h"
 
+#define FREE_ALL      \
+    free_bignum(fst); \
+    free_bignum(snd); \
+    free_bignum(res)
+
+#define TEST_ADD_EQ(sign_fst, length_fst, sign_snd, length_snd, ans_str) \
+    bignum_t *fst = init_bignum_mods(sign_fst, length_fst);              \
+    bignum_t *snd = init_bignum_mods(sign_snd, length_snd);              \
+    bignum_t *res = to_bignum(ans_str);                                  \
+    mu_check(is_equal(add(fst, snd), res));                              \
+    FREE_ALL
+
 MU_TEST(add_negative_same_length)
 {
-    bignum_t *fst = init_bignum_mods(1, 5);
-    bignum_t *snd = init_bignum_mods(1, 5);
-    unsigned int *res_digits = malloc(5 * sizeof(unsigned int));
-    res_digits[0] = 0;
-    res_digits[1] = 9;
-    res_digits[2] = 6;
-    res_digits[3] = 4;
-    res_digits[4] = 2;
-
-    bignum_t *res = init_bignum_digits(1, res_digits, 5);
-    bignum_t *add_res = add(fst, snd);
-
-    mu_check(is_equal(add(fst, snd), res));
-
-    free_bignum(fst);
-    free_bignum(snd);
-    free_bignum(res);
-    free_bignum(add_res);
+    TEST_ADD_EQ(1, 5, 1, 5, "-24690");
 }
 
 MU_TEST(add_positive_same_length)
 {
-    bignum_t *fst = init_bignum_mods(0, 15);
-    bignum_t *snd = init_bignum_mods(0, 15);
-    bignum_t *res = to_bignum("246913578024690");
-    bignum_t *add_res = add(fst, snd);
-
-    mu_check(is_equal(add(fst, snd), res));
-
-    free_bignum(fst);
-    free_bignum(snd);
-    free_bignum(res);
-    free_bignum(add_res);
+    TEST_ADD_EQ(0, 15, 0, 15, "246913578024690");
 }
 
 MU_TEST(add_negative)
-{
-    bignum_t *fst = to_bignum("-12345");
-    bignum_t *snd = to_bignum("-54321");
-    bignum_t *res = to_bignum("-66666");
-    bignum_t *add_res = add(fst, snd);
-
-    mu_check(is_equal(add_res, res));
-
-    free_bignum(fst);
-    free_bignum(snd);
-    free_bignum(res);
-    free_bignum(add_res);
+{ 
+    TEST_ADD_EQ(1, 5, 1, 4, "-13579");
 }
 
 MU_TEST(add_positive)
 {
-    bignum_t *fst = to_bignum("12345");
-    bignum_t *snd = to_bignum("54321");
-    bignum_t *res = to_bignum("66666");
-    bignum_t *add_res = add(fst, snd);
-
-    mu_check(is_equal(add_res, res));
-
-    free_bignum(fst);
-    free_bignum(snd);
-    free_bignum(res);
-    free_bignum(add_res);
+    TEST_ADD_EQ(0, 3, 0, 11, "12345679024");
 }
 
 MU_TEST_SUITE(add_tests)
